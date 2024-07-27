54767ddafa87cb7b0977078f2a72aa5d28565471
Srigowri13 <sgowri699@gmail.com>
2024-07-27 18:47:16 +0530

    Admin Page without CSS

---
+++ expa-frontend/src/Components/AdminPage/NoCampsMessage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NoCampsMessage.css'; // Import CSS file for styling
+
+const NoCampsMessage = () => {
+  return (
+    <div className="no-camps-message">
+      No camps created yet.
+    </div>
+  );
+};
+
+export default NoCampsMessage;
