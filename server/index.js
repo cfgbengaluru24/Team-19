ab760f4dd98390fa1f30345e16c6f9b5a702fcc9
Sudesh Nayak <sudeshnayak@pop-os.localdomain>
2024-07-28 04:48:24 +0530

    added csv

--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@ const connectDB = require("./configs/mongodb.config");
 const dotenv = require("dotenv");
 const cookieParser = require('cookie-parser');
 const cors=require('cors');
+const fs = require("fs");
+const path = require("path");
+const csv = require("csv-parser");
 
 dotenv.config();
 
@@ -16,6 +19,20 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.static('public'));
+
+app.get("/getFile", (req, res) => {
+    const results = [];
+  
+    fs.createReadStream(path.join(__dirname, "sample.csv"))
+      .pipe(csv())
+      .on("data", (data) => results.push(data))
+      .on("end", () => {
+        res.json(results); // Send the JSON result to the frontend
+      })
+      .on("error", (error) => {
+        res.status(500).send(error.message); // Handle any errors
+      });
+  });
 
 app.use('/user',userRouter);
 app.use('/trainee',assess_voiceRouter);
